3ddfaf624e27869589556e3ab96178620f913d15
Herve <hervemashukane@gmail.com>
2025-08-28 09:38:38 +0100

    generate dashboard widjets and get ready to use them independantly

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "./navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [
-    NavbarComponent, 
-    SidebarComponent, 
-    FooterComponent, 
-    RouterOutlet
-  ],
+  imports: [RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
