d9f2aab4756412209c8fbf7acad42991a843d863
Herve <hervemashukane@gmail.com>
2025-09-03 10:11:53 +0100

    render dashboard table dynamically

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  patientActivities: any = [
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'September 2, 2025', status: 'Completed'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'September 2, 2025', status: 'Pending'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'September 2, 2025', status: 'Canceled'},
+    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'September 2, 2025', status: 'Completed'},
+    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'September 2, 2025', status: 'Pending'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'September 2, 2025', status: 'Canceled'},
+  ]
 }
