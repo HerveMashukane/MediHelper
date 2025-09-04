7ff931d10f82a05a218b6656072efb242e5550af
Herve <hervemashukane@gmail.com>
2025-09-04 14:18:00 +0100

    build my doctors page

--- src/app/pages/mydoctors/mydoctors.component.ts
+++ src/app/pages/mydoctors/mydoctors.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-mydoctors',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './mydoctors.component.html',
   styleUrl: './mydoctors.component.css'
 })
 export class MydoctorsComponent {
-
+  doctors: any = [
+    {
+      preferedName: 'Dr. Herve', 
+      fullName: 'Dr. Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Dr. Herve Mashukane', 
+      speaciality: 'Generalist', 
+      viewBtn: 'View', 
+      contactBtn: 'Contact'
+    },
+    {
+      preferedName: 'Dr. Herve', 
+      fullName: 'Dr. Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Dr. Herve Mashukane', 
+      speaciality: 'Cardiologist', 
+      viewBtn: 'View', 
+      contactBtn: 'Contact'
+    },
+    {
+      preferedName: 'Dr. Herve', 
+      fullName: 'Dr. Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Dr. Herve Mashukane', 
+      speaciality: 'Dermatologist', 
+      viewBtn: 'View', 
+      contactBtn: 'Contact'
+    },
+    {
+      preferedName: 'Dr. Herve', 
+      fullName: 'Dr. Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Dr. Herve Mashukane', 
+      speaciality: 'Neurologist', 
+      viewBtn: 'View', 
+      contactBtn: 'Contact'
+    },
+    {
+      preferedName: 'Dr. Herve', 
+      fullName: 'Dr. Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Dr. Herve Mashukane', 
+      speaciality: 'Surgerist', 
+      viewBtn: 'View', 
+      contactBtn: 'Contact'
+    },
+    {
+      preferedName: 'Dr. Herve', 
+      fullName: 'Dr. Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Dr. Herve Mashukane', 
+      speaciality: 'Oncologist', 
+      viewBtn: 'View', 
+      contactBtn: 'Contact'
+    },
+  ]
 }
