235d8063b618b7de46a0b6de2bee09025cbdda5f
Herve <hervemashukane@gmail.com>
2025-09-05 17:13:43 +0100

    add pageFlip animation to all pages to improve UX

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        slideInUp: {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+        pageFlip: {
+          '0%': { transform: 'rotateY(90deg)', opacity: '0' },
+          '100%': { transform: 'rotateY(0deg)', opacity: '1' },
         },
       },
       animation: {
-        slideInUp: 'slideInUp 0.5s ease-out forwards',
+        pageFlip: 'pageFlip 0.5s ease-out forwards',
       },
     },
   },
