4486a5c5675baa8d0e3916ec7d423a78d4800fdd
Herve <hervemashukane@gmail.com>
2025-09-05 12:14:23 +0100

    implement talwind slideInUp animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
-    /** @type {import('tailwindcss').Config} */
-    module.exports = {
-      content: [
-        "./src/**/*.{html,ts}",
-      ],
-      theme: {
-        extend: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        slideInUp: {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
       },
-      plugins: [],
-    }
+      animation: {
+        slideInUp: 'slideInUp 0.5s ease-out forwards',
+      },
+    },
+  },
+  plugins: [],
+}
