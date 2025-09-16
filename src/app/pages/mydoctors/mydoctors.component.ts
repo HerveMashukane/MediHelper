694eeda96f9f01528c015f88d9eea610edee2b34
Herve <hervemashukane@gmail.com>
2025-09-16 11:55:37 +0100

    toggle form logic implementation

--- src/app/pages/mydoctors/mydoctors.component.ts
+++ src/app/pages/mydoctors/mydoctors.component.ts
@@ -17,63 +17,10 @@ export class MydoctorsComponent {
   constructor(private doctorsService: MydoctorsService) {
     this.doctors$ = this.doctorsService.doctors$;
   }
-  // ngOnInit(): void {
-  //   this.doctorsService.doctors$.subscribe
-  // }
-  // doctors: any = [
-  //   {
-  //     preferedName: 'Dr. Christelle', 
-  //     fullName: 'Dr. Christelle Pelaya', 
-  //     image: '/assets/images/herve.png', 
-  //     alt: 'Dr. Christelle Pelaya', 
-  //     speciality: 'Generalist', 
-  //     viewBtn: 'View', 
-  //     contactBtn: 'Contact'
-  //   },
-  //   {
-  //     preferedName: 'Dr. Herve', 
-  //     fullName: 'Dr. Herve Mashukane', 
-  //     image: '/assets/images/herve.png', 
-  //     alt: 'Dr. Herve Mashukane', 
-  //     speciality: 'Cardiologist', 
-  //     viewBtn: 'View', 
-  //     contactBtn: 'Contact'
-  //   },
-  //   {
-  //     preferedName: 'Dr. Hiro', 
-  //     fullName: 'Dr. Hiro Mataba', 
-  //     image: '/assets/images/herve.png', 
-  //     alt: 'Dr. Hiro Mataba', 
-  //     speciality: 'Dermatologist', 
-  //     viewBtn: 'View', 
-  //     contactBtn: 'Contact'
-  //   },
-  //   {
-  //     preferedName: 'Dr. Christelle', 
-  //     fullName: 'Dr. Christelle Pelaya', 
-  //     image: '/assets/images/herve.png', 
-  //     alt: 'Dr. Christelle Pelaya', 
-  //     speciality: 'Neurologist', 
-  //     viewBtn: 'View', 
-  //     contactBtn: 'Contact'
-  //   },
-  //   {
-  //     preferedName: 'Dr. Herve', 
-  //     fullName: 'Dr. Herve Mashukane', 
-  //     image: '/assets/images/herve.png', 
-  //     alt: 'Dr. Herve Mashukane', 
-  //     speciality: 'Surgerist', 
-  //     viewBtn: 'View', 
-  //     contactBtn: 'Contact'
-  //   },
-  //   {
-  //     preferedName: 'Dr. Hiro', 
-  //     fullName: 'Dr. Hiro Mataba', 
-  //     image: '/assets/images/herve.png', 
-  //     alt: 'Dr. Hiro Mataba', 
-  //     speciality: 'Oncologist', 
-  //     viewBtn: 'View', 
-  //     contactBtn: 'Contact'
-  //   },
-  // ]
+
+  // toggle form
+  isFormVisible: boolean = false;
+  toggleForm() {
+    this.isFormVisible = !this.isFormVisible;
+  }
 }
