7f37ab49edd7b9d7f6c2581b0937c4d49b0da842
Herve <hervemashukane@gmail.com>
2025-09-18 10:46:33 +0100

    animate modal doctors details

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,14 @@ module.exports = {
           '0%': { transform: 'rotateY(90deg)', opacity: '0' },
           '100%': { transform: 'rotateY(0deg)', opacity: '1' },
         },
+        fadeIn: {
+          '0%': {transform: 'translateY(-100%)', opacity: '0'},
+          '100%': {transform: 'translateY(0%)', opacity: '1'},
+        }
       },
       animation: {
         pageFlip: 'pageFlip 0.5s ease-out forwards',
+        fadeIn: 'fadeIn 0.5s ease-in forwards',
       },
     },
   },
