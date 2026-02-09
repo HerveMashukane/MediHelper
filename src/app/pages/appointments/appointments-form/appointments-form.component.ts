ccec33eff8600501f5547761a29d815273551543
Herve <hervemashukane@gmail.com>
2026-02-09 19:58:40 +0100

    generate appointments form

---
+++ src/app/pages/appointments/appointments-form/appointments-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointments-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './appointments-form.component.html',
+  styleUrl: './appointments-form.component.css'
+})
+export class AppointmentsFormComponent {
+
+}
