fcb328bf80f77d94f52674b687c45171dd7eac22
HerveMashukane <hervemashukane@gmail.com>
2026-03-19 22:04:37 +0200

    update settings

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  activeTab:string = 'profile'
 
 }
