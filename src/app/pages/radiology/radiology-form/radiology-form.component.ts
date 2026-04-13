b75044e8efc922babb407e21a1d252c73fe30118
HerveMashukane <hervemashukane@gmail.com>
2026-04-13 22:11:41 +0200

    create radiology form component

---
+++ src/app/pages/radiology/radiology-form/radiology-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radiology-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './radiology-form.component.html',
+  styleUrl: './radiology-form.component.css'
+})
+export class RadiologyFormComponent {
+
+}
--- src/app/pages/radiology/radiology.component.ts
+++ src/app/pages/radiology/radiology.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
+import { RadiologyFormComponent } from './radiology-form/radiology-form.component'
 
 export interface RadiologyExam {
 
@@ -20,7 +21,7 @@ export interface RadiologyExam {
 @Component({
   selector: 'app-radiology',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [RadiologyFormComponent, CommonModule, FormsModule],
   templateUrl: './radiology.component.html',
   styleUrls: ['./radiology.component.css']
 })
