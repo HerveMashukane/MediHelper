1bafd98263b13620511bf2b1a9ac06cce594f563
HerveMashukane <hervemashukane@gmail.com>
2026-04-24 14:29:05 +0200

    show appointment length by status

--- src/app/pages/appointments/appointments.component.ts
+++ src/app/pages/appointments/appointments.component.ts
@@ -15,12 +15,36 @@ export class AppointmentsComponent {
 
   appointments$: Observable<Appointment[]>;
   // appointment stats for KPI cards
+  appointmentStats$: Observable<{
+    Active: number;
+    Upcoming: number;
+    Completed: number;
+    Pending: number;
+    Canceled: number;
+
+  }>;
   isFormVisible: boolean = false;
 
   constructor(private appointmentService: AppointmentService) {
     this.appointments$ = this.appointmentService.appointments$;
-
     // Reactive KPI stats
+    this.appointmentStats$ = this.appointments$.pipe(
+      map((appointments) => {
+        const stats = {
+          Active: 0,
+          Upcoming: 0,
+          Completed: 0,
+          Pending: 0,
+          Canceled: 0
+        };
+        for(let app of appointments) {
+          if(stats[app.status as keyof typeof stats] !== undefined) {
+            stats[app.status as keyof typeof stats]++;
+          }
+        }
+        return stats;
+      })
+    )
   }
   toggleFormVisibility() {
     this.isFormVisible = !this.isFormVisible;
--- src/app/services/appointments/appointment.service.ts
+++ src/app/services/appointments/appointment.service.ts
@@ -23,10 +23,50 @@ export class AppointmentService {
     {
       id: 1,
       patientName: 'Ava Richardson',
-      doctorName: 'Dr. Sarah Williams',
+      doctorName: 'Dr. William Smith',
       date: '2026-04-11',
+      time: '09:00',
+      status: 'Active',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 2,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-10',
       time: '09:30',
       status: 'Upcoming',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 3,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-09',
+      time: '08:30',
+      status: 'Completed',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 4,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-08',
+      time: '09:00',
+      status: 'Pending',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 5,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-07',
+      time: '09:00',
+      status: 'Canceled',
       type: 'Consultation',
       message: 'First visit for general checkup'
     },
