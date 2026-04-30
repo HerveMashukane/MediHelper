f2ec817adc168b4d32ac877545b3b59964ce5cf2
HerveMashukane <hervemashukane@gmail.com>
2026-04-30 19:51:08 +0200

    small fix: render appoitnments length per status on head title

--- src/app/services/appointments/appointment.service.ts
+++ src/app/services/appointments/appointment.service.ts
@@ -22,56 +22,56 @@ export class AppointmentService {
   }
   
   public appointmentsSource = new BehaviorSubject<Appointment[]>([
-    // {
-    //   id: 1,
-    //   patientName: 'Ava Richardson',
-    //   doctorName: 'Dr. William Smith',
-    //   date: '2026-04-11',
-    //   time: '09:00',
-    //   status: 'Active',
-    //   type: 'Consultation',
-    //   message: 'First visit for general checkup'
-    // },
-    // {
-    //   id: 2,
-    //   patientName: 'Ava Richardson',
-    //   doctorName: 'Dr. William Smith',
-    //   date: '2026-04-10',
-    //   time: '09:30',
-    //   status: 'Upcoming',
-    //   type: 'Consultation',
-    //   message: 'First visit for general checkup'
-    // },
-    // {
-    //   id: 3,
-    //   patientName: 'Ava Richardson',
-    //   doctorName: 'Dr. William Smith',
-    //   date: '2026-04-09',
-    //   time: '08:30',
-    //   status: 'Completed',
-    //   type: 'Consultation',
-    //   message: 'First visit for general checkup'
-    // },
-    // {
-    //   id: 4,
-    //   patientName: 'Ava Richardson',
-    //   doctorName: 'Dr. William Smith',
-    //   date: '2026-04-08',
-    //   time: '09:00',
-    //   status: 'Pending',
-    //   type: 'Consultation',
-    //   message: 'First visit for general checkup'
-    // },
-    // {
-    //   id: 5,
-    //   patientName: 'Ava Richardson',
-    //   doctorName: 'Dr. William Smith',
-    //   date: '2026-04-07',
-    //   time: '09:00',
-    //   status: 'Canceled',
-    //   type: 'Consultation',
-    //   message: 'First visit for general checkup'
-    // },
+    {
+      id: 1,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-11',
+      time: '09:00',
+      status: 'Active',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 2,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-10',
+      time: '09:30',
+      status: 'Upcoming',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 3,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-09',
+      time: '08:30',
+      status: 'Completed',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 4,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-08',
+      time: '09:00',
+      status: 'Pending',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
+    {
+      id: 5,
+      patientName: 'Ava Richardson',
+      doctorName: 'Dr. William Smith',
+      date: '2026-04-07',
+      time: '09:00',
+      status: 'Canceled',
+      type: 'Consultation',
+      message: 'First visit for general checkup'
+    },
   ]);
   // get observable list of appointments
   appointments$ = this.appointmentsSource.asObservable();
