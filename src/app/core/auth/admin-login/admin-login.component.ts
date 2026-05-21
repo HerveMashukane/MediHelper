8cb9e07aecab5d82ab1d3fe44e82f8f375089a1f
HerveMashukane <hervemashukane@gmail.com>
2026-05-21 10:51:04 +0200

    hardcoded admin login credentials prparing for real auth implementatuin

--- src/app/core/auth/admin-login/admin-login.component.ts
+++ src/app/core/auth/admin-login/admin-login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css'
 })
 export class AdminLoginComponent {
+  
+  constructor(private router: Router) {}
+  // login variables
+  adminEmail = "herve@gmail.com";
+  adminPassword = "1234";
+  errorMessage = "";
 
+  adminLogin() {
+    const email = this.adminEmail.trim();
+    const password = this.adminPassword.trim();
+    if (email && password) {
+      this.router.navigate(["adminDashboard"]);
+    } else {
+      this.errorMessage = "Invalid credentials";
+    }
+  }
 }
